81853403287932a022b7cc09bee81e74d7c054fc
Alexander Fengler <alexanderfengler@gmx.de>
2020-10-23 13:16:04 -0400

    ..

--- hddm/models/__init__.py
+++ hddm/models/__init__.py
@@ -7,12 +7,12 @@ from .hddm_regression import HDDMRegressor
 from .hddm_rl import HDDMrl
 from .rl import Hrl
 from .hddm_nn import HDDMnn
-from .hddm_nn_weibull import HDDMnn_weibull
-from .hddm_nn_angle import HDDMnn_angle
-from .hddm_nn_regression import HDDMnnRegressor
-from .hddm_nn_levy import HDDMnn_levy
-from .hddm_nn_ornstein import HDDMnn_ornstein
-from .hddm_nn_new import HDDMnn_new
+#from .hddm_nn_weibull import HDDMnn_weibull
+#from .hddm_nn_angle import HDDMnn_angle
+#from .hddm_nn_regression import HDDMnnRegressor
+#from .hddm_nn_levy import HDDMnn_levy
+#from .hddm_nn_ornstein import HDDMnn_ornstein
+#from .hddm_nn_new import HDDMnn_new
 #from .hddm_nn_new import HDDMnn_new
 #from .hddm_nn_weibull_regression import HDDMnnWeibullRegressor
 
