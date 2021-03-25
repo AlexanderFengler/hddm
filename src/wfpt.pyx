741977d817832648531f0f821d50f633458aa5e8
Alexander Fengler <alexanderfengler@gmx.de>
2021-03-25 01:19:55 -0400

    ..

--- src/wfpt.pyx
+++ src/wfpt.pyx
@@ -203,8 +203,8 @@ def wiener_like_nn_ddm_pdf(np.ndarray[float, ndim = 1] x,
                            double t, #  double st,
                            double p_outlier = 0, 
                            double w_outlier = 0,
-                           bint logp = 0,
-                           **kwargs):
+                           bint logp = 0)
+                           # **kwargs):
 
     # double err = 1e-4, 
     # int n_st = 10, 
@@ -214,7 +214,7 @@ def wiener_like_nn_ddm_pdf(np.ndarray[float, ndim = 1] x,
     cdef np.ndarray[float, ndim = 1] y = np.empty(size, dtype=np.float32)
 
     cdef Py_ssize_t size = x.shape[0]
-    cdef float log_p
+    cdef np.ndarray[float, ndim = 1] log_p = np.zeros(size, dtype = np.float32)
     cdef int n_params = 4
     cdef float ll_min = -16.11809
     cdef np.ndarray[float, ndim = 2] data = np.zeros((size, n_params + 2), dtype = np.float32)
