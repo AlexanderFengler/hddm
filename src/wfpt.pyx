6c4fab23d32626088a0370a8622cb8ab143fb13e
Alexander Fengler <alexanderfengler@gmx.de>
2021-03-25 17:21:04 -0400

    ..

--- src/wfpt.pyx
+++ src/wfpt.pyx
@@ -226,14 +226,14 @@ def wiener_like_nn_ddm_pdf(np.ndarray[float, ndim = 1] x,
     
     # Call to network:
     if p_outlier == 0: # ddm_model
-        log_p = np.core.umath.maximum(network.predict_on_batch(data), ll_min)
+        log_p = np.squeeze(np.core.umath.maximum(network.predict_on_batch(data), ll_min))
     else: # ddm_model
-        log_p = np.log(np.exp(np.core.umath.maximum(network.predict_on_batch(data), ll_min)) * (1.0 - p_outlier) + (w_outlier * p_outlier))
+        log_p = np.squeeze(np.log(np.exp(np.core.umath.maximum(network.predict_on_batch(data), ll_min)) * (1.0 - p_outlier) + (w_outlier * p_outlier)))
         #log_p = np.sum(np.log(np.add(np.multiply(np.exp(np.core.umath.maximum(ddm_model.predict_on_batch(data), ll_min)), 
         #                    (1.0 - p_outlier)), 
         #              (w_outlier * p_outlier))))
     if logp == 0:
-        log_p = np.exp(log_p) # shouldn't be called log_p anymore but no need for an extra array here
+        log_p = np.squeeze(np.exp(log_p)) # shouldn't be called log_p anymore but no need for an extra array here
     return log_p
 
 def wiener_like_nn_ddm_analytic(np.ndarray[float, ndim = 1] x, 
