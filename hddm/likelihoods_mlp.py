79c5aba26b8208dab32f9b5de3f1022f3f5c1850
Alexander Fengler <alexanderfengler@gmx.de>
2021-03-27 00:25:57 -0400

    ..

--- hddm/likelihoods_mlp.py
+++ hddm/likelihoods_mlp.py
@@ -49,6 +49,8 @@ def make_mlp_likelihood_complete(model, **kwargs):
             print(self.parents.value)
             print('trying to print the value part of parents')
             print(dict(self.parents.value))
+            print('tying to pring the values part of parents')
+            print(self.parents.values)
             return partial(simulator, model = model, n_samples = self.shape, max_t = 20) # This may still be buggy !
 
         def pdf_ddm(self, x):
