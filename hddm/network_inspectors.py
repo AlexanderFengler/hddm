2ab1ad8fb469a16f1105814351f64908d190015f
Alexander Fengler <alexanderfengler@gmx.de>
2021-04-08 14:43:05 -0400

    ..

--- hddm/network_inspectors.py
+++ hddm/network_inspectors.py
@@ -21,9 +21,28 @@ from copy import deepcopy
 
 
 def get_mlp(model = 'angle'):
+    """ Returns the keras network which is the basis of the MLP likelihoods
+
+    :Arguments:
+        model: str <default='angle'>
+        Specifies the models you would like to load
+    Returns:
+        keras.model: You can call a forward pass through the model via .predict_on_batch()
+    """
     network = load_mlp(model = model)
-    return network
+    return network.predict_on_batch
 
 def get_cnn(model = 'angle', nbin = 512):
+    """ Returns tensorflow CNN which is the basis of the CNN likelihoods
+
+    :Arguments:
+        model: str <default='angle'>
+        Specifies the models you would like to load
+    Returns:
+        function: 
+            Returns a function that you can call passing as an argument a 1d or 2d np.array with datatype np.float32.
+            The shape of the input to this function should match the number of parameter vectors (rows) and the corresponding parameters (cols).
+                
+    """
     network = load_cnn(model = model, nbin = nbin)
     return network
