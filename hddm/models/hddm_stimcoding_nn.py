617d7b41eb9123c0487ea79082f7d7c6c8c72e14
Alexander Fengler <alexanderfengler@gmx.de>
2021-04-10 21:14:20 -0400

    ..

--- hddm/models/hddm_stimcoding_nn.py
+++ hddm/models/hddm_stimcoding_nn.py
@@ -6,6 +6,7 @@ from kabuki.hierarchical import Knode
 from kabuki.utils import stochastic_from_dist
 from hddm.models import HDDM
 from hddm.keras_models import load_mlp
+from hddm.cnn.wrapper import load_cnn
 import hddm
 
 import wfpt
