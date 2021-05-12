0565269c302cd0614b698a26be36250c752e3374
Alexander Fengler <alexanderfengler@gmx.de>
2021-05-12 16:14:43 -0400

    fix parameter names for simulators

--- hddm/simulators/hddm_dataset_generators.py
+++ hddm/simulators/hddm_dataset_generators.py
@@ -36,7 +36,11 @@ def hddm_preprocess(simulator_data = None, subj_id = 'none', keep_negative_respo
     # Add ground truth parameters to dataframe
     for param in model_config[simulator_data[2]['model']]['params']:
         print(simulator_data[2][param])
-        df[param] = simulator_data[2][param]
+        print(type(simulator_data[2][param]))
+        if len(simulator_data[2][param]) > 1:
+            df[param] = simulator_data[2][param]
+        else:
+            df[param] = simulator_data[2][param]
     return df
 
 def str_to_num(string = '', n_digits = 3):
