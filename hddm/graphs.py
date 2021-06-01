7d2daad43abfc10675fcb885402f7ec7c3a018f8
Alexander Fengler <alexanderfengler@gmx.de>
2021-06-01 00:03:50 -0400

    bug fixing

--- hddm/graphs.py
+++ hddm/graphs.py
@@ -118,7 +118,10 @@ def make_trace_plotready_h_c(trace_dict = None,
             # Dat gt_parameter_vector to dat_h_c dict 
             # If parameters not in the dataframe --> set to None
             if test_passed:
-                dat_h_c[key][subj_id]['gt_parameter_vector'] = dat_h_c[key][subj_id]['data'].loc[0, :][[param for param in model_config[model_ground_truth]['params']]].values
+                print('progressive testing')
+                print(dat_h_c[key][subj_id]['data'])
+                print( dat_h_c[key][subj_id]['data'].loc[0, :])
+                dat_h_c[key][subj_id]['gt_parameter_vector'] = dat_h_c[key][subj_id]['data'].iloc[0, :][[param for param in model_config[model_ground_truth]['params']]].values
                 #x.loc[0, :][['one', 'two']].values
             else: 
                 dat_h_c[key][subj_id]['gt_parameter_vector'] = None
