34184be11466371d7f9da0f59a89940975a3b5eb
Alexander Fengler <alexanderfengler@gmx.de>
2021-06-01 00:18:10 -0400

    bug fixing

--- hddm/graphs.py
+++ hddm/graphs.py
@@ -461,7 +461,7 @@ def _make_plot_sub_data(data = None, plot_n = None, multi_subject = None, multi_
             grouped_sub_data = {}
             grouped_sub_data[0] = {}
             grouped_sub_data[0]['traces'] = np.vstack([sub_data[key]['traces'] for key in sub_data.keys()])
-            grouped_sub_data[0]['data'] = pd.concatenta([sub_data[key]['data'] for key in sub_data.keys()], axis = 0)
+            grouped_sub_data[0]['data'] = pd.concatenate([sub_data[key]['data'] for key in sub_data.keys()], axis = 0)
             # We don't have a real ground truth parameter vector for grouped data
             grouped_sub_data[0]['gt_parameter_vector'] = None
             grouped_sub_data[0]['cond_subj_label'] = sub_data['cond_subj_label']
